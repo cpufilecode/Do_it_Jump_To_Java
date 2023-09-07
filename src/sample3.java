c72bdc238dc3d617ac674bb162cdffd44f88fb23
cpufilecode <zf0908@naver.com>
2023-09-07 23:17:23 +0900

    upload

--- src/sample3.java
+++ src/sample3.java
@@ -1,2 +1,43 @@
 public class sample3 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(7%3);
+        System.out.println(3%7);
+
+        int i=0;
+        int j=10;
+
+        // i++;
+        // j--;
+
+        System.out.println(i);
+        System.out.println(j);
+
+        System.out.println(i++);
+        System.out.println(i);
+
+        System.out.println(++i);
+        System.out.println(i);
+
+        char a1='a';
+        char a2=97;
+        char a3='\u0061';
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+
+        String ab ="happy java";
+        String bb="a";
+        String bc="123";
+
+        System.out.println(ab);
+        System.out.println(bb);
+        System.out.println(bc);
+    }
 }
